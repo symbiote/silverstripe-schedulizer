a78cfe1c56dfee2babe886ff5165abf8e581da9d
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-10-04 15:34:03 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/ScheduleRange.php
+++ code/ScheduleRange.php
@@ -9,7 +9,21 @@
  */
 class ScheduleRange extends DataObject {
 
-	private static $db = array (
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+	
+    private static $table_name = 'ScheduleRange';
+
+    private static $db = array (
 		'Title'				=> 'VarChar',
 		//Seconds between scheduled times
 		'Interval'			=> 'Int',
